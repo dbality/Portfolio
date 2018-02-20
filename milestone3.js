6185c5d4feb9144815e9fa4dd045fb4db7e99158
dbality <35548863+dbality@users.noreply.github.com>
2018-02-19 19:38:07 -0700

    Update milestone3.js

--- milestone3.js
+++ milestone3.js
@@ -1,3 +1,4 @@
+var map;
 function initialize () {
 	var Norway = new google.maps.LatLng(60.4720, 8.4689)
 	
@@ -6,5 +7,7 @@ function initialize () {
 		center: Norway,
 		mapTypeId: google.maps.MapTypeId.HYBRID
 	};
-	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	 map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
