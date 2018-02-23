f0e4c678bf6999fb80117bcb0f4f8d2bcae45741
dbality <35548863+dbality@users.noreply.github.com>
2018-02-23 01:23:24 -0700

    Update milestone4.2.js

--- milestone4.2.js
+++ milestone4.2.js
@@ -3,7 +3,7 @@ function initialize () {
 	var Norway = new google.maps.LatLng(60.4720, 8.4689)
 	
 	var mapOptions = {
-		zoom: 10,
+		zoom: 7,
 		center: Norway,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		styles: [
