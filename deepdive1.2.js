19adf48b6ce7912a4e02c5b199d535e1aae9c218
dbality <35548863+dbality@users.noreply.github.com>
2018-02-26 18:19:33 -0700

    Update and rename deepdive1.1.js to deepdive1.2.js

--- deepdive1.2.js
+++ deepdive1.2.js
@@ -1,10 +1,10 @@
 var map;
 function initialize () {
-	var Norway = new google.maps.LatLng(60.4720, 8.4689)
+	var Norway = new google.maps.LatLng(60.4720, 8.4689);
 	
-	var Fantoft = new google.maps.LatLng(60.339167, 5.353333)
+	var Fantoft = new google.maps.LatLng(60.339167, 5.353333);
 	
-	var Holmenkollen =new google.maps.LatLng(59.965664, 10.672328)
+	var Holmenkollen =new google.maps.LatLng(59.965664, 10.672328);
 	
 	var burntchurch1 = new google.maps.Marker({
 		position: Fantoft,
