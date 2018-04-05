4824c00478043439190c844b34b8b7a435239694
dbality <35548863+dbality@users.noreply.github.com>
2018-04-04 18:18:38 -0600

    Update and rename OpenLayer1.1.js to OpenLayer1.2.js

---
+++ OpenLayer1.2.js
@@ -0,0 +1,14 @@
+// OpenLayer1.1.js
+
+var myMap = new.ol.Map({
+	target: 'map',
+	layers: [
+		new ol.layer.Tile({
+			source: new ol.source.OSM()
+		})
+	],
+	view: new ol.View({
+		center: ol.proj.fromLonLat([0,0]),
+		zoom: 8
+		})
+	});
